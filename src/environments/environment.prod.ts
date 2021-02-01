1028cfaabc748f2726bd892bc50369557f2438fc
Leandro Gustavo Neis <lgn200@gmail.com>
2021-02-01 14:41:10 -0300

    Correcao link backend heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://prova-softplan-pessoa-api.herokuapp.com',
+  apiUrl: 'https://leandroneis-pessoa-api.herokuapp.com',
 
-  tokenWhitelistedDomains: [ new RegExp('prova-softplan-pessoa-api.herokuapp.com') ],
+  tokenWhitelistedDomains: [ new RegExp('leandroneis-pessoa-api.herokuapp.com') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
